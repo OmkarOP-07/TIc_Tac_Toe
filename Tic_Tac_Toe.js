f3a77b5c5ea9ea61811f38f5dd634db2e21fd82d
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-04-17 23:26:56 +0530

    made some changes

--- Tic_Tac_Toe.js
+++ Tic_Tac_Toe.js
@@ -1,60 +1,85 @@
-let rstbtn = document.querySelector("#reset-btn")
-let boxes = document.querySelectorAll(".box")
-let winmsg = document.querySelector(".winmsg")
+let rstbtn = document.querySelector("#reset-btn");
+let boxes = document.querySelectorAll(".box");
+let winmsg = document.querySelector(".winmsg");
+let scoreOEl = document.getElementById("scoreO");
+let scoreXEl = document.getElementById("scoreX");
+let playerONameEl = document.getElementById("playerOName");
+let playerXNameEl = document.getElementById("playerXName");
+
 let turnO = true;
 
+// Prompt for player names
+let oname = prompt("Enter Player 1 Name (O)");
+let xname = prompt("Enter Player 2 Name (X)");
+if (!oname) oname = "Player O";
+if (!xname) xname = "Player X";
+
+// Set names on the screen
+playerONameEl.innerText = oname;
+playerXNameEl.innerText = xname;
+
+// Track scores
+let scoreO = 0;
+let scoreX = 0;
+
 let win_pattern = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        console.log("box is clicked")
-        if (turnO) {
-            box.innerText = "O"
-            turnO = false;
-        } else {
-            box.innerText = "X"
-            turnO = true
-        }
-        box.disabled = true; // Don't allow multiple box click
-        checkWinner();
-    })
+  box.addEventListener("click", () => {
+    if (box.innerText !== "") return;
+
+    box.innerText = turnO ? "O" : "X";
+    box.disabled = true;
+    checkWinner();
+    turnO = !turnO;
+  });
 });
 
-function delay(ms){
-    return new Promise(resolve => setTimeout(resolve,ms));
-}
-let  rstEvent = () => {
-    boxes.forEach((box) => {
-        box.innerText = ""
-    })
-    winmsg.classList.add("hide")
-}
-
-winmsg.classList.add("hide")
-
 const checkWinner = () => {
-    for(let pattern of win_pattern) {
-        let pos1val = boxes[pattern[0]].innerText;
-        let pos2val = boxes[pattern[1]].innerText;
-        let pos3val = boxes[pattern[2]].innerText;
+  for (let pattern of win_pattern) {
+    let [a, b, c] = pattern;
+    let valA = boxes[a].innerText;
+    let valB = boxes[b].innerText;
+    let valC = boxes[c].innerText;
 
-        if(pos1val != "" && pos2val != "" && pos3val != "" ){
-            if(pos1val == pos2val && pos2val == pos3val){
-                console.log("Winner", pos1val)
-                winmsg.innerText = `Congractulations, Winner is ${pos1val}`
-                winmsg.classList.remove("hide") 
-            }
-        }
+    if (valA && valA === valB && valB === valC) {
+      const winnerName = valA === "O" ? oname : xname;
+
+      winmsg.innerText = `Congratulations, Winner is ${winnerName}`;
+      winmsg.classList.remove("hide");
+
+      if (valA === "O") {
+        scoreO++;
+        scoreOEl.innerText = scoreO;
+      } else {
+        scoreX++;
+        scoreXEl.innerText = scoreX;
+      }
+
+      boxes.forEach((box) => box.disabled = true);
+      return;
     }
-}
+  }
+};
 
-rstbtn.addEventListener("click", rstEvent)
+const rstEvent = () => {
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.disabled = false;
+  });
+  winmsg.classList.add("hide");
+};
+
+rstbtn.addEventListener("click", rstEvent);
+
+// Initially hide the win message
+winmsg.classList.add("hide");
